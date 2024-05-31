693e57578bd3654457fa754aec1f7ecbd39ea4cf
sid@747 <sidmm747@gmail.com>
2024-05-31 23:54:30 +0530

    Added image Style shadow and bg image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <img class="img-fluid" style="width: 100px; height: 70px" alt="Vue logo" src="./assets/image.png" />
+  <div class="image-container">
+    <img class="img-fluid" style="width: 100px; height: 70px" alt="Vue logo" src="./assets/image.png" />
+  </div>
   <AppHeader />
   <router-view />
   <!-- <AppFooter /> -->
@@ -21,18 +23,37 @@ export default {
 
 <style>
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   font-family: "Merriweather Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #ffffff;
-  /* margin-top: 60px; */
   background-color: #cccccc;
-  background-image: url("./assets/bg.png");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
+  background: url(https://res.cloudinary.com/ritalbradley/image/upload/v1657644013/pexels-elijah-o_donnell-3473569_o7ohfd.jpg) center/cover no-repeat fixed;
+
+}
+
+.image-container {
+  display: inline-block;
+  position: relative;
+}
+
+.img-fluid {
+  width: 100px;
+  height: 70px;
+  position: relative;
+  z-index: 1;
+}
+
+.image-container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url('./assets/image.png') no-repeat center/contain;
+  filter: drop-shadow(0px 4px 6px rgba(251, 230, 136, 0.715));
+  z-index: 0;
 }
 </style>
